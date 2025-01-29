69db960aee0e7f7af30ce66fa8ac2879a2048e0b
Dhivyaprakash S S <dhivyaprakash192@gmail.com>
2025-01-29 23:25:11 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+document.getElementById('register').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('cardContainer').classList.add('flip');
+
+    setTimeout(function() {
+        window.location.href = 'signup.html';
+    }, 600); // Wait for the flip animation before navigating
+});
